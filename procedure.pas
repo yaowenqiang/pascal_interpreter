ca902431dc9ef9c64a40627382708aecfdcf6bde
Yaowenqiang <yaowenqiang111@163.com>
2021-10-08 22:04:57 +0800

    add double procedure pas

---
+++ procedure.pas
@@ -0,0 +1,15 @@
+program Main;
+    var x, y : real;
+    procedure AlphaA(a : integer);
+        var y : integer;
+    begin { AlphaA }
+    end; { AlphaA }
+
+    procedure AlphaB(a : integer);
+        var b : integer;
+    begin { AlphaA }
+    end; { AlphaA }
+begin { Main }
+end . { Main }
+
+
